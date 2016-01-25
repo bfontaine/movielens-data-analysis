14ec95f2c9d5d5213f013114ed805c1f169fec90
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-25 17:17:58 +0100

    __eq__ & cached values added on models

--- movies/db.py
+++ movies/db.py
@@ -17,27 +17,42 @@ class BaseModel(peewee.Model):
     class Meta:
         database = db
 
+    def post_import(self):
+        # should be redefined by child classes
+        pass
+
+
 class Movie(BaseModel):
     movie_id = IntegerField(unique=True, primary_key=True)
     title = CharField()
     # TODO change these to DateFields
-    release_date = DateTimeField(null=True) # should that really be allowed?
+    release_date = DateTimeField(null=True) # should null really be allowed?
     video_release_date = DateTimeField(null=True)
     imdb_url = CharField()
 
-    def has_genre(self, genre):
-        # TESTME
-        return getattr(self, Movie.genre_attr(genre))
-
-    def set_genres(self, genres):
-        # TESTME
-        for g, v in zip(GENRES, genres):
-            attr = Movie.genre_attr(g)
-            setattr(self, attr, bool(int(v)))
+    # cached values
+    ratings_count = IntegerField(null=True)
 
     @classmethod
     def genre_attr(cls, g):
         return "genre_%s" % g.lower().replace("-", "_").replace("'", "")
+
+    def genres(self):
+        return [g for g in GENRES if self.has_genre(g)]
+
+    def has_genre(self, genre):
+        return getattr(self, Movie.genre_attr(genre))
+
+    def set_genres(self, genres):
+        for g, v in zip(GENRES, genres):
+            attr = Movie.genre_attr(g)
+            setattr(self, attr, bool(int(v)))
+
+    def post_import(self):
+        self.ratings_count = len(self.ratings)
+
+    def __eq__(self, other):
+        return isinstance(other, Movie) and self.movie_id == other.movie_id
 
 class User(BaseModel):
     user_id = IntegerField(unique=True, primary_key=True)
@@ -46,11 +61,26 @@ class User(BaseModel):
     occupation = CharField()
     zip_code = CharField()
 
+    # cached values
+    ratings_count = IntegerField(null=True)
+
+    def post_import(self):
+        self.ratings_count = len(self.ratings)
+
+    def __eq__(self, other):
+        return isinstance(other, User) and self.user_id == other.user_id
+
+
 class Rating(BaseModel):
     user = ForeignKeyField(User, related_name="ratings")
     movie = ForeignKeyField(Movie, related_name="ratings")
     rating = IntegerField()
     date = DateTimeField()
+
+    def __eq__(self, other):
+        return self.user == other.user and \
+                self.movie == other.movie and \
+                self.date == other.date
 
 for g in GENRES:
     # http://stackoverflow.com/a/22365143/735926
